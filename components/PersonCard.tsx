f84c15a62d43d9cee8986bcc85d6353c586e0785
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-06-11 19:28:22 -0700

    props person card

--- components/PersonCard.tsx
+++ components/PersonCard.tsx
@@ -3,18 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Person } from "types";
 
-const PersonCard = ({
-  person: {
-    id,
-    name,
-    created_at,
-    age,
-    description,
-    lastSeen,
-    photoUrl,
-    status,
-  },
-}: Person[]) => {
+const PersonCard = (props: Person) => {
   const sburl =
     "https://vudbxrkcwwrwjoetsfzm.supabase.co/storage/v1/object/public/images/";
 
@@ -22,36 +11,36 @@ const PersonCard = ({
   return (
     <div className="card outline w-96 ">
       <div className="card-body">
-        <p>{format(new Date(created_at), "MM/dd/yy  eee h:mm a")} </p>
+        <p>{format(new Date(props.created_at), "MM/dd/yy  eee h:mm a")} </p>
         <h1 className="card-title">
           {" "}
-          <span className={spanClass}>Name:</span> {name}
+          <span className={spanClass}>Name:</span> {props.name}
         </h1>
         <p>
           {" "}
-          <span className={spanClass}>Age:</span> {age}
+          <span className={spanClass}>Age:</span> {props.age}
         </p>
         <p>
           <span className={spanClass}>Description:</span>
-          {description}
+          {props.description}
         </p>
         <p>
           <span className={spanClass}>Last Seen:</span>
 
-          {lastSeen}
+          {props.lastSeen}
           {status}
         </p>
-        {photoUrl && (
+        {props.photoUrl && (
           <div className="relative h-[14rem]">
             <Image
               layout="fill"
               objectFit="contain"
-              src={`${sburl}/${photoUrl}`}
+              src={`${sburl}/${props.photoUrl}`}
             />
           </div>
         )}
         <div className="card-actions justify-end">
-          <Link href={`/missing/${id}`}>
+          <Link href={`/missing/${props.id}`}>
             <a className="btn btn-primary">More Info</a>
           </Link>
         </div>
