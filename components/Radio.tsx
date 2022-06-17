a9266370bab8ca8f1006b038625f772595ae66b7
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-06-17 13:50:02 -0700

    missing graphql

--- components/Radio.tsx
+++ components/Radio.tsx
@@ -2,7 +2,7 @@ import { Code } from "types";
 
 export default function Radio({ radio }: Code) {
   return (
-    <div className="radio-div text-sm">
+    <div className="radio-div ">
       <p className="font-bold">{radio.name}</p>
       <p>{radio.desc}</p>
     </div>
