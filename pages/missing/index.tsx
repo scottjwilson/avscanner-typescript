c8439a61962919bd9ad38a2d62a1bfcec53c866e
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-06-30 18:26:30 -0700

    testing rq missing persons hook

--- pages/missing/index.tsx
+++ pages/missing/index.tsx
@@ -1,36 +1,25 @@
 import Loading from "@/components/Loading";
 import PersonCard from "@/components/PersonCard";
-import { supabaseClient } from "@supabase/supabase-auth-helpers/nextjs";
+import usePersons from "hooks/usePersons";
 import type { NextPage } from "next";
 import { NextSeo } from "next-seo";
-import Link from "next/link";
-import { useState } from "react";
-import { useQuery } from "react-query";
 
 const MissingIndex: NextPage = () => {
-  const [persons, setPersons] = useState("");
-  async function fetchPersons() {
-    const res = await supabaseClient.from("missing_persons").select("*");
-    return res;
-  }
-
-  function Persons() {
-    const { data: persons, status } = useQuery("posts", fetchPersons, {
-      staleTime: 0,
-      // cacheTime: 10,
-    });
-    if (status === "loading") {
-      return <Loading />;
-    }
-    if (status === "error") {
-      return <p>Error...</p>;
-    }
-    if (status === "success") {
-      return (
-        <>
-          {" "}
+  const personsQuery = usePersons();
+  const title = `${process.env.NEXT_PUBLIC_SITE_TITLE} -  Missing Persons`;
+  const description = "AV Scanner News Missing Persons";
+  return (
+    <>
+      <NextSeo title={title} description={description} />
+      <section className="max-w-xl mx-auto px-4">
+        <h1 className="text-center mb-4">Missing Persons</h1>
+        {personsQuery.isLoading ? (
+          <Loading />
+        ) : personsQuery.isError ? (
+          <span>Something went wrong</span>
+        ) : (
           <div className="flex flex-col items-center w-full ">
-            {persons.data.map((person, i) => {
+            {personsQuery.data.map((person, i) => {
               return (
                 <PersonCard
                   key={i}
@@ -41,20 +30,7 @@ const MissingIndex: NextPage = () => {
               );
             })}
           </div>
-        </>
-      );
-    }
-  }
-
-  return (
-    <>
-      <NextSeo
-        title={`${process.env.NEXT_PUBLIC_SITE_TITLE} -  Missing Persons`}
-        description="AV Scanner News Missing Persons"
-      />
-      <section className="max-w-xl mx-auto px-4">
-        <h1 className="text-center mb-4">Missing Persons</h1>
-        <Persons />
+        )}
       </section>
     </>
   );
