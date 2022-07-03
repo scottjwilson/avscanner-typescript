8aa0fa54eccc6eae990cc6657450b66695a828d9
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-07-03 07:47:01 -0700

    testing og image

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,17 +5,35 @@ import { Post } from "types";
 import usePosts from "hooks/usePosts";
 import Loading from "@/components/Loading";
 
+const title = `${process.env.NEXT_PUBLIC_SITE_TITLE} - Home`;
+const description = "Community safety through citizen awareness";
+const url = `${process.env.NEXT_PUBLIC_CLIENT_URL}`;
+const imageUrl = `${process.env.NEXT_PUBLIC_CLIENT_URL}/og.jpg`;
+const alt = `${process.env.NEXT_PUBLIC_SITE_TITLE} - Home`;
+
 const Home: NextPage = () => {
   const postsQuery = usePosts();
 
   return (
     <>
       <NextSeo
-        title={`${process.env.NEXT_PUBLIC_SITE_TITLE} - Home`}
-        description="Community safety through citizen awareness"
+        title={title}
+        description={description}
+        openGraph={{
+          url: url,
+          title: title,
+          description: description,
+          images: [
+            {
+              url: imageUrl,
+              width: 800,
+              height: 600,
+              alt: alt,
+              type: "image/jpeg",
+            },
+          ],
+        }}
       />
-
-      {/* <pre>{JSON.stringify(postsQuery, null, 2)}</pre> */}
       <section className="max-w-xl mx-auto px-4">
         {postsQuery.isLoading ? (
           <Loading />
