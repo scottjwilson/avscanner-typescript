34f07d56c1f84aa2ef4167e28740de72e506cf24
Scott Wilson <35642678+scottjwilson@users.noreply.github.com>
2022-07-07 07:56:11 -0700

    light and business daisy

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  daisyui: {
+    themes: ["light", "business"],
+  },
   theme: {
     extend: {
       fontFamily: {
